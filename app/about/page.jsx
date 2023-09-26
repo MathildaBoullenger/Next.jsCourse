eb3b698071ad8b4982b5bc18e3b5ea7651239972
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-09-27 00:38:02 +1300

    up to section 3

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>
+          This will be the About Us page.
+        </p>
+      </>
+    );
+  }
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from "react";
+
+export default function HomePage() {
+    /*useEffect(() => {
+        window.alert('Welcome to my site!!!');
+        }, []);*/
+  return (
+    <>
+      <h1>Indie Gamer</h1>
+      <p>
+        Only the best indie games, reviewed for you.
+        </p>
+    </>
+  );
+}
+ 
